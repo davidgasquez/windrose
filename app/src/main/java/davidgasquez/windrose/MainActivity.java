8a0603728956300c7bac68faf35a948416ed1a93
David Gasquez <davidgasquez@gmail.com>
2014-12-14 12:29:36 +0100

    Create compass. Bug with North direction!

--- app/src/main/java/davidgasquez/windrose/MainActivity.java
+++ app/src/main/java/davidgasquez/windrose/MainActivity.java
@@ -1,19 +1,111 @@
 package davidgasquez.windrose;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements SensorEventListener {
+
+    TextView northDirection;
+    private ImageView image;
+    private float currentDegree = 0f;
+    private SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.imageViewCompass);
+        northDirection = (TextView) findViewById(R.id.northDirection);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mSensorManager.unregisterListener(this);
+    }
+
+    /**
+     * Called when sensor values have changed.
+     * <p>See {@link android.hardware.SensorManager SensorManager}
+     * for details on possible sensor types.
+     * <p>See also {@link android.hardware.SensorEvent SensorEvent}.
+     * <p/>
+     * <p><b>NOTE:</b> The application doesn't own the
+     * {@link android.hardware.SensorEvent event}
+     * object passed as a parameter and therefore cannot hold on to it.
+     * The object may be part of an internal pool and may be reused by
+     * the framework.
+     *
+     * @param event the {@link android.hardware.SensorEvent SensorEvent}.
+     */
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // get the angle around the z-axis rotated
+        float objetiveDegree = Math.round(event.values[0]);
+
+        northDirection.setText("objetiveDegree" + Float.toString(objetiveDegree)
+                + "\ncurrentDegree" + Float.toString(currentDegree)
+                + "\nabs" + Float.toString(Math.abs(objetiveDegree + currentDegree)));
+
+        //if (objetiveDegree > 0 && objetiveDegree < 180) {
+        //    northDirection.setText("Turn left");
+        //}
+        //else {
+        //    northDirection.setText("Turn right");
+        //}
+
+        RotateAnimation rotation;
+        rotation = new RotateAnimation(
+                currentDegree,
+                -objetiveDegree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+
+        rotation.setDuration(300);
+        rotation.setFillAfter(true);
+
+        image.startAnimation(rotation);
+        currentDegree = -objetiveDegree;
+
+    }
+
+    /**
+     * Called when the accuracy of the registered sensor has changed.
+     * <p/>
+     * <p>See the SENSOR_STATUS_* constants in
+     * {@link android.hardware.SensorManager SensorManager} for details.
+     *
+     * @param sensor   Sensor type
+     * @param accuracy The new accuracy of this sensor, one of
+     *                 {@code SensorManager.SENSOR_STATUS_*}
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
